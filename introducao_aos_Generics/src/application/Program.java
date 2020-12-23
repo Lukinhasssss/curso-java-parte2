3fb9679b3d16158b9d963267db49e5b56bafe776
Lucas Monteiro <lukasmonteir0@hotmail.com>
2020-12-23 13:27:20 -0300

    Atualização de: Introdução aos Generics

--- introducao_aos_Generics/src/application/Program.java
+++ introducao_aos_Generics/src/application/Program.java
@@ -1,10 +1,52 @@
 package application;
 
+import java.util.Scanner;
+
+import services.PrintService;
+
 public class Program {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Hello World!");
+		/*
+		 *	• Generics permitem que classes, interfaces e métodos possam ser parametrizados
+		 *	  por tipo. Seus benefícios são:
+		 *	    • Reuso
+		 *	    • Type safety
+		 *	    • Performance
+		 *
+		 *	• Uso comum: coleções
+		 *
+		 *	  List<String> list = new ArrayList<>();
+		 *	  list.add("Maria");
+		 *	  String name = list.get(0);
+		 */
+		
+		/*
+		 * 	Exemplo utilizando generics
+		 * 
+		 * 	  Deseja-se fazer um programa que leia uma quantidade M, e depois N números inteiros.
+		 * 	Ao final, imprima esses números de forma organizada. Em seguida, informar qual foi o
+		 * 	primeiro valor informado.
+		 */
+		
+		Scanner sc = new Scanner(System.in);
+		
+		PrintService<Integer> printService = new PrintService<>();
+		
+		System.out.print("How many values? ");
+		int n = sc.nextInt();
+		
+		for (int i = 0; i < n; i++) {
+			int value = sc.nextInt();
+			printService.addValue(value);
+		}
+		
+		printService.print();
+		Integer x = printService.first();
+		System.out.println("First: " + x);
+		
+		sc.close();
 
 	}
 
